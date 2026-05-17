e3df4de4fd7077cb7d8ba05e818c82f8fd6c5428
Nguyen Nguyen <nasongnguyen@gmail.com>
2026-05-17 15:14:46 +0300

    update timer basic (tim6 and tim7)

---
+++ 06-timer-basic/Inc/main.h
@@ -0,0 +1,22 @@
+/*
+ * main.h
+ *
+ *  Created on: 14.5.2026
+ *      Author: nn
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <dac.h>
+#include "nucleo446start.h"
+#include <stdio.h>
+
+#include "tim6.h"
+#include "tim7.h"
+#include "uart2.h"
+#include "adc.h"
+#include "systick.h"
+#include "interrupt_user_button.h"
+
+#endif /* MAIN_H_ */
---
+++ 06-timer-basic/Src/main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int main(void) {
+	SystemClock_Config();
+	SystemCoreClockUpdate();
+	fpu_enable();
+	Tim6_init();
+	Tim7_init();
+	Uart2_init();
+	ADC_Init();
+	ADC_Enable();
+	DAC_Init_PA4();
+
+	char buf[40];
+	int count = 0;
+
+	while (1) {
+
+		sprintf(buf, "count = %d\n\r", count);
+		UART2_SendString(buf);
+
+		count++;
+		if (count >= 100)
+			count = 0;
+
+		delay_s(1);
+	}
+
+}
+
