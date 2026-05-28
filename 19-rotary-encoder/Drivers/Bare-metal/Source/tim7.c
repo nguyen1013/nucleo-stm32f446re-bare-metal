0bcd2f573d1ed21ff73510fc7336894d7e927b83
Nguyen Nguyen <nasongnguyen@gmail.com>
2026-05-28 09:01:52 +0300

    refactor rotary encoder driver

--- 19-rotary-encoder/Drivers/Bare-metal/Source/tim7.c
+++ 19-rotary-encoder/Drivers/Bare-metal/Source/tim7.c
@@ -9,37 +9,34 @@
 
 volatile uint32_t g_tim7_ms = 0;
 
-void Tim7_init(void)
-{
-    RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
+void Tim7_init(void) {
+	RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
 
-    TIM7->CR1 &= ~TIM_CR1_CEN;
+	TIM7->CR1 &= ~TIM_CR1_CEN;
 
-    /* 90MHz / (8999+1) = 10kHz */
-    TIM7->PSC = 8999;
+	/* 90MHz / (8999+1) = 10kHz */
+	TIM7->PSC = 8999;
 
-    /* 10kHz / (9+1) = 1kHz => 1ms */
-    TIM7->ARR = 9;
+	/* 10kHz / (9+1) = 1kHz => 1ms */
+	TIM7->ARR = 9;
 
-    TIM7->CNT = 0;
-    TIM7->EGR = TIM_EGR_UG;
+	TIM7->CNT = 0;
+	TIM7->EGR = TIM_EGR_UG;
 
-    TIM7->SR &= ~TIM_SR_UIF;
+	TIM7->SR &= ~TIM_SR_UIF;
 
-    /* enable interrupt */
-    TIM7->DIER |= TIM_DIER_UIE;
+	/* enable interrupt */
+	TIM7->DIER |= TIM_DIER_UIE;
 
-    NVIC_EnableIRQ(TIM7_IRQn);
+	NVIC_EnableIRQ(TIM7_IRQn);
 
-    TIM7->CR1 |= TIM_CR1_CEN;
+	TIM7->CR1 |= TIM_CR1_CEN;
 }
 
-void TIM7_IRQHandler(void)
-{
-    if (TIM7->SR & TIM_SR_UIF)
-    {
-        TIM7->SR &= ~TIM_SR_UIF;
+void TIM7_IRQHandler(void) {
+	if (TIM7->SR & TIM_SR_UIF) {
+		TIM7->SR &= ~TIM_SR_UIF;
 
-        g_tim7_ms++;
-    }
+		g_tim7_ms++;
+	}
 }
